29c0923f2158e7afddf7cb19cfe107234b5433f3
Akshay Yelle <02b3akshay@gmail.com>
2024-08-30 09:07:19 +0530

    resetting the project ⚓

--- app/(tabs)/artists/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function ArtistsScreen() {
-	return (
-		<View className="bg-black w-full h-screen">
-			<Text className="text-white text-base font-geist">ArtistsScreen</Text>
-		</View>
-	);
-}
--- app/(tabs)/playlists/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function PlaylistsScreen() {
-	return (
-		<View className="bg-black w-full h-screen">
-			<Text className="text-white text-base font-geist">PlaylistsScreen</Text>
-		</View>
-	);
-}
